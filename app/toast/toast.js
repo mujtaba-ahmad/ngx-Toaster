c0cd76748296a9039eed424be98b0b41788739c9
Mujtaba <mmuujji@gmail.com>
2016-09-26 13:48:48 +0500

    changed logic

--- app/toast/toast.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=toast.js.map
--- app/toast/toast.ts
+++
@@ -1,9 +0,0 @@
-export interface IToast {
-  id: number;
-  title: string;
-  msg: string;
-  showClose: boolean;
-  type: string;
-  theme: string;
-  timeout: number;
-}
