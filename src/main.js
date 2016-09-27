b6358fcd7ff17bc808628f687b6556ff548c9722
furqan haroon <ranaabdullah539@gmail.com>
2016-09-27 19:04:50 +0500

    extra files removed

--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var toaster_module_1 = require('./toaster.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(toaster_module_1.ToasterModule);
-//# sourceMappingURL=main.js.map
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { ToasterModule } from './toaster.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(ToasterModule);
