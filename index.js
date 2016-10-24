6bbbdb481890c1cab50ca9694e35b19d87cdc1a9
Mujtaba <mmuujji@gmail.com>
2016-10-24 19:21:24 +0500

    changes index file

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./src/toaster-app.component'));
 __export(require('./src/toast/toast.component'));
 __export(require('./src/toast/toaster.component'));
 __export(require('./src/toast/toaster.service'));
-__export(require('./src/toaster.module'));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-export * from './src/toaster-app.component';
+
 export * from './src/toast/Itoast';
 export * from './src/toast/toast.component';
 export * from './src/toast/toaster.component';
 export * from './src/toast/toaster.service';
-export * from './src/toaster.module';
