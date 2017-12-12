f287a6a1ee09c368632a411eb6d8274da519fab0
Hammad Maqbool <hammad417@gmail.com>
2017-12-12 17:30:11 +0500

    Create new project for toaster plugin, andd added new module toaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ToasterModule } from "./toaster/toaster.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToasterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
