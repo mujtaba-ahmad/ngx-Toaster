09940ea54209c681797cd4f5921739996f18eeb6
hammad417 <hammad417@gmail.com>
2018-01-10 18:53:37 +0500

    Removed src folder

--- src/toast/Itoast.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=Itoast.js.map
--- src/toast/Itoast.ts
+++
@@ -1,8 +0,0 @@
-export interface Itoast {
-  id: number;
-  title: string;
-  msg: string;
-  showClose: boolean;
-  type: string;
-  timeout: number;
-}
